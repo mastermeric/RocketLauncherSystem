175c786d2f0e2279db81ad22e7f174aa1b73b809
mastermeric <49819371+mastermeric@users.noreply.github.com>
2022-05-06 15:57:54 +0300

    Add files via upload

---
+++ ConsoleMultiThread/Models/TelemetryTCPModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMultiThread
+{
+    public class TelemetryTCPModel
+    {
+        public TelemetryTCPModel()
+        {
+            //Gerekli startup islemleri..
+        }
+
+        public string ip { get; set; }
+        public int port { get; set; }
+    }
+}

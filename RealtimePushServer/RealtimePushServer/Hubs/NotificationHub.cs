cf43bbbba4017e421ed1a37591abef1ef8dbda09
mastermeric <49819371+mastermeric@users.noreply.github.com>
2022-05-06 15:58:33 +0300

    Add files via upload

---
+++ RealtimePushServer/RealtimePushServer/Hubs/NotificationHub.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Threading.Tasks;
+
+namespace RealtimePushServer.Hubs
+{
+    public class NotificationHub:Hub
+    {
+
+        public async Task SendMessage(string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", message);
+            Console.WriteLine("message ==>" + message);
+        }
+
+        public async Task SendMessageToAll(string gonderen, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", gonderen, message);
+            Console.WriteLine("message ==>" + gonderen + " -> " + message);
+        }
+
+        public async Task CheckVersion(float newX, float newY)
+        {
+            await Clients.Others.SendAsync("mySignalFonksiyon", newX, newY);
+            //await Clients.All.SendAsync("mySignalFonksiyon", newX, newY);
+            Console.WriteLine("Bilgi Geldi..." + newX + " VE " + newY);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            //Set value in Session object.
+            //var httpContext = Context.GetHttpContext();
+            //httpContext.Session.SetString("Person", "Mudassar");
+
+            await Clients.Caller.SendAsync("ReceiveMessageFromServerHub", "Server says :" + this.Context.ConnectionId + " is conencted now.\n");
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            Clients.All.SendAsync("ReceiveMessageFromServerHub", "Server says :" + this.Context.ConnectionId + " is disconencted !\n");
+            return base.OnDisconnectedAsync(exception);
+        }
+    }
+}
